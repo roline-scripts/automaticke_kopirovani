fc0ac128ced6aaefd14fa4b5fd02f86884c27f5f
necron <xdxd@xd.com>
2025-03-18 13:10:59 +0100

    Imporved file processing.

--- bin/TextProcessor.py
+++ bin/TextProcessor.py
@@ -5,10 +5,8 @@ import os
 # * Nezáleží tedy na tom, zda jsou všechna naskenovaná čísla umístěna v jednom souboru, nebo jsou rozdělena do více souborů.
 
 class DataManager():
-    def __init__(self, file_name: str): # enter file name as it is in the default input folder
-        self.DEFAULT_INPUT_DIR = "../input"
-        self.FILE_PATH= self.DEFAULT_INPUT_DIR + "/" + file_name
-        self.FILE = open(self.FILE_PATH, "r").read()
+    def __init__(self): # enter file name as it is in the default input folder
+        self.DEFAULT_INPUT_DIR = "../input/" # ! Za název složky je vždy nutné přidat lomítko!
 
         self.file_list = os.listdir(self.DEFAULT_INPUT_DIR)
         
@@ -16,24 +14,31 @@ class DataManager():
         self.SN_PATTERN = r"\d{17}\s\d{5}"   
         self.DELIVERY_ID_PATTERN = r"\d{18}"
         self.AMOUNT = r"(?<=;)\d{2}(?=;)"
+
+        # Processed data
         self.serial_nums = []
         self.delivery_id = []
         self.amount = []
 
-    def processFile(self): 
-        self.serial_nums = self.matchPattern(self.SN_PATTERN)
-        self.delivery_id = self.matchPattern(self.DELIVERY_ID_PATTERN)
-        self.amount = self.matchPattern(self.AMOUNT)
+    def processFiles(self): 
+        input_data = ""
+        for file in self.file_list:
+            with open(self.DEFAULT_INPUT_DIR + file, "r") as f:
+                input_data += f.read()
 
-    def matchPattern(self, exp):
-        found = re.findall(exp, self.FILE)
+        self.serial_nums = self.matchPattern(self.SN_PATTERN, input_data)
+        self.delivery_id = self.matchPattern(self.DELIVERY_ID_PATTERN, input_data)
+        self.amount = self.matchPattern(self.AMOUNT, input_data)
+
+    def matchPattern(self, exp, data):
+        found = re.findall(exp, data)
         return found
 
 def main():
     test_path = "test1.txt"
-    file = DataManager(test_path)
-    print(file.file_list)
-    file.processFile()
+    file = DataManager()
+    print(file.serial_nums)
+    file.processFiles()
     for i in file.serial_nums:
         print(i)
 
